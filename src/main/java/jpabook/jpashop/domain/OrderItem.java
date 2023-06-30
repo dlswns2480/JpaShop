c2ba0a2b1abbb2add6b1587e75459a766a1bd26b
dlswns2480 <dlswns2480@naver.com>
2023-06-30 19:35:43 +0900

    Item entity 생성

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,4 +1,23 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
 public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Item item;
+
+    private int orderPrice;
+    private int count;
 }
