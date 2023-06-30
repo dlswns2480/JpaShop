ed396ec7bb4300ee3b1977a156b0daa452047f0e
dlswns2480 <dlswns2480@naver.com>
2023-06-30 18:48:15 +0900

    값 타입인 Address 생성(Member, Delievery에서 쓰임)

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,16 +1,21 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String name;
