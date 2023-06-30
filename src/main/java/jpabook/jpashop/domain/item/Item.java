0256ec9dfc5ff0b7056841ff3685833ac9e17107
dlswns2480 <dlswns2480@naver.com>
2023-06-30 19:48:54 +0900

    Item의 카테고리들 Entity 생성

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,14 +1,13 @@
-package jpabook.jpashop.domain;
+package jpabook.jpashop.domain.item;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) //Item 테이블에 다 때려박는 형식(상속 관계 설정)
+@DiscriminatorColumn(name = "dtype")
 public abstract class Item {
 
     @Id @GeneratedValue
