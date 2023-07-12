f9e3f2b141334a0ed3d05d0de4b6b736ed27434e
dlswns2480 <dlswns2480@naver.com>
2023-07-12 14:12:46 +0900

    상품 등록 컨트롤러 및 템플릿 추가

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -44,7 +44,6 @@ public class MemberController {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
         return "members/memberList";
-
     }
 
 }
